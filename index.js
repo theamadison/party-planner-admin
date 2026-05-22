fa2a7e9ce1766643a88942b7647e9580f68dc603
theamadison <alfoxthea@gmail.com>
2026-05-21 22:51:02 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // === Constants ===
 const BASE = "https://fsa-crud-2aa9294fe819.herokuapp.com/api";
-const COHORT = ""; // Make sure to change this!
+const COHORT = "/2604-THEA";
 const API = BASE + COHORT;
 
 // === State ===
@@ -10,12 +10,19 @@ let rsvps = [];
 let guests = [];
 
 /** Updates state with all parties from the API */
+/** Because we are making a request that takes time to respond, our function must be asynchronous */
 async function getParties() {
+  /**  Since we are working with a Promise, we have to try making the request */
   try {
+    /** The term `await` means wait for the promise to resolve */
+    /** The term `fetch` means make GET request to specified path */
     const response = await fetch(API + "/events");
+    /** After the resoponse comes back, I have parse the JSON into an actual object */
     const result = await response.json();
     parties = result.data;
+    /** We have to make sure the website is showing the updated version, so therfore we have to `render` */
     render();
+    /** In case there are any errors, we must catch the error in advance to prevent our website from crashing */
   } catch (e) {
     console.error(e);
   }
@@ -52,6 +59,35 @@ async function getGuests() {
     const result = await response.json();
     guests = result.data;
     render();
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+/** Creates a party via the API */
+async function addParty(party) {
+  try {
+    await fetch(API + "/events", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(party),
+    });
+    await getParties();
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+/** Deletes the party with the given ID via the API */
+async function deleteParty(id) {
+  try {
+    await fetch(API + "/events/" + id, {
+      method: "DELETE",
+    });
+    selectedParty = undefined;
+    await getParties();
   } catch (e) {
     console.error(e);
   }
@@ -102,8 +138,12 @@ function SelectedParty() {
     <address>${selectedParty.location}</address>
     <p>${selectedParty.description}</p>
     <GuestList></GuestList>
+    <button>Delete party</button>
   `;
   $party.querySelector("GuestList").replaceWith(GuestList());
+
+  const $delete = $party.querySelector("button");
+  $delete.addEventListener("click", () => deleteParty(selectedParty.id));
 
   return $party;
 }
@@ -113,8 +153,8 @@ function GuestList() {
   const $ul = document.createElement("ul");
   const guestsAtParty = guests.filter((guest) =>
     rsvps.find(
-      (rsvp) => rsvp.guestId === guest.id && rsvp.eventId === selectedParty.id
-    )
+      (rsvp) => rsvp.guestId === guest.id && rsvp.eventId === selectedParty.id,
+    ),
   );
 
   // Simple components can also be created anonymously:
@@ -128,6 +168,43 @@ function GuestList() {
   return $ul;
 }
 
+/** Form that allows users to input information about a new party */
+function NewPartyForm() {
+  const $form = document.createElement("form");
+  $form.innerHTML = `
+    <label>
+      Name
+      <input name="name" required />
+    </label>
+    <label>
+      Description
+      <input name="description" required />
+    </label>
+    <label>
+      Date
+      <input name="date" type="date" required />
+    </label>
+    <label>
+      Location
+      <input name="location" required />
+    </label>
+    <button>Add party</button>
+  `;
+  $form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const data = new FormData($form);
+    const date = new Date(data.get("date")).toISOString();
+    addParty({
+      name: data.get("name"),
+      description: data.get("description"),
+      date,
+      location: data.get("location"),
+    });
+  });
+
+  return $form;
+}
+
 // === Render ===
 function render() {
   const $app = document.querySelector("#app");
@@ -137,6 +214,8 @@ function render() {
       <section>
         <h2>Upcoming Parties</h2>
         <PartyList></PartyList>
+        <h3>Add a new party</h3>
+        <NewPartyForm></NewPartyForm>
       </section>
       <section id="selected">
         <h2>Party Details</h2>
@@ -146,6 +225,7 @@ function render() {
   `;
 
   $app.querySelector("PartyList").replaceWith(PartyList());
+  $app.querySelector("NewPartyForm").replaceWith(NewPartyForm());
   $app.querySelector("SelectedParty").replaceWith(SelectedParty());
 }
 
